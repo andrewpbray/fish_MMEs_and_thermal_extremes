945707cdeb0a1b8107b0cccec353f5c0e358423d
Andrew Bray <abray@rstudio-dev.reed.edu>
2019-01-30 11:47:14 -0800

    add ribbons, points, text annotations, tweak sizing.

--- figures/summerkill_timeseries.R
+++ figures/summerkill_timeseries.R
@@ -10,7 +10,7 @@ compute_quantile <- function(x, q, reps = 1000) {
     quantile(q)
 }
 
-# Load data and model
+# Load data
 setwd("../data/processed")
 historical_data <- read_csv("historical_data.csv",
                             col_types = list(wbic = col_character(),
@@ -22,145 +22,81 @@ historical_data <- read_csv("historical_data.csv",
   mutate(summerkill = fct_recode(as.factor(summerkill),
                                  "neg" = "0",
                                  "pos" = "1"))
+future_data <- read_csv('future_data.csv')
 
-setwd("../prepared_for_modeling")
-future <- read_csv("future.csv",
-                   col_types = list(ice_duration = col_double()))
-setwd("../models")
-predictive_model <- read_rds("predictive_lasso.rds")
+# Lasso selected logistic model
+m1 <- glm(summerkill ~ temp + population + lat,
+          data =  historical_data,
+          family = binomial)
 
-# Extract predictions
-predictions <- predict(object = predictive_model,
-                       newdata = future, 
-                       type = "prob")$pos
+p <- predict(object = m1, 
+             newdata = future_data, 
+             type = "response")
 
 # Prepare data for plot
-f_data <- future %>%
-  add_column(prob = predictions) %>%
+f_data <- future_data %>%
+  add_column(prob = p) %>%
   group_by(year) %>%
   summarize(temp       = mean(mean_surf),
             kills      = sum(prob),
-            lb_kill    = compute_quantile(prob, q = .025, reps = 100),
-            ub_kill    = compute_quantile(prob, q = .975, reps = 100)
+            lb_kill    = compute_quantile(prob, q = .025, reps = 500),
+            ub_kill    = compute_quantile(prob, q = .975, reps = 500)
             ) 
 
 f_early <- f_data %>%
   filter(year < 2075) %>%
   mutate(kills_smooth = loess(kills ~ year, .)$fitted,
-         lb_smooth    = loess(kills ~ year, .)$fitted,
-         ub_smooth    = loess(kills ~ year, .)$fitted)
+         lb_smooth    = loess(lb_kill ~ year, .)$fitted,
+         ub_smooth    = loess(ub_kill ~ year, .)$fitted)
 
 f_late <- f_data %>%
   filter(year > 2075) %>%
   mutate(kills_smooth = loess(kills ~ year, .)$fitted,
-         lb_smooth    = loess(kills ~ year, .)$fitted,
-         ub_smooth    = loess(kills ~ year, .)$fitted)
+         lb_smooth    = loess(lb_kill ~ year, .)$fitted,
+         ub_smooth    = loess(ub_kill ~ year, .)$fitted)
 
 hist_data <- historical_data %>% 
   filter(year < 2014) %>%
   group_by(year) %>%
   summarize(temp = mean(mean_surf),
-            kills = sum(summerkill == "pos")) %>%
-  mutate(kills_smooth = loess(kills ~ year, .)$fitted)
+            kills_obs = sum(summerkill == "pos")) %>%
+  mutate(kills_obs_smooth = loess(kills_obs ~ year, .)$fitted)
 
 gap_data <- tibble(year = c(2014:2040, 2060:2080))
 
 full_data <- f_early %>%
   bind_rows(f_late, hist_data, gap_data)
 
-
 # Construct timeseries plot
 p_timeseries <- ggplot(full_data, aes(x = year)) +
-  geom_rect(xmin = 2015, xmax = 2040, ymin = 0, ymax = 110, fill = "lightgray", alpha = .002) +
-  geom_rect(xmin = 2060, xmax = 2080, ymin = 0, ymax = 110, fill = "gray", alpha = .002) +
+  geom_point(aes(y = kills_obs)) +
+  geom_ribbon(aes(ymin = lb_smooth, ymax = ub_smooth), fill = "steelblue", alpha = .2) +
   geom_line(aes(y = kills_smooth)) +
-  geom_line(aes(y = lb_smooth), linetype = "dashed") +
-  geom_line(aes(y = ub_smooth), linetype = "dashed") +
+  annotate("rect", xmin = 2014, xmax = 2041, ymin = 0, ymax = 35, fill = "gray", alpha = .15) +
+  annotate("rect", xmin = 2059, xmax = 2080, ymin = 0, ymax = 35, fill = "gray", alpha = .15) +
+  #geom_line(aes(y = lb_smooth), linetype = "dashed") +
+  #geom_line(aes(y = ub_smooth), linetype = "dashed") +
+  annotate("text", label = "Historical", x = 2007, y = 36, size = 3) +
+  annotate("text", label = "Early 21st Cent.", x = 2050, y = 36, size = 3) +
+  annotate("text", label = "Late 21st Cent.", x = 2090, y = 36, size = 3) +
   ylab("Total Predicted Summerkills") +
   xlab(NULL) +
+  ylim(c(0, 36)) +
   theme_bw()
-
 
 # Temperature plot
 p_temp <- full_data %>%
   ggplot(aes(x = year, y = 1, fill = temp)) +
-  geom_tile() + 
-  scale_fill_distiller(type = "seq", palette = "Reds", direction = 1) +
-  theme_void()
+  geom_tile(na.rm = TRUE) + 
+  scale_fill_distiller(type = "seq", palette = "Reds", 
+                       direction = 1, na.value = "#e3e3e3") +
+  labs(fill = "Temp") +
+  theme_void() +
+  guides(fill = guide_colourbar(title.theme = element_text(size = 8),
+                                label.theme = element_text(size = 7),
+                                barwidth = .8,
+                                barheight = 4))
 
-p_timeseries + p_temp + plot_layout(ncol = 1, heights = c(12, 1))
-
-
-
-
-
-make_quantiles <- function(x, probs = seq(0, 1, by = .2)) {
-  qu <- quantile(x, probs = probs)
-  cut(x, 
-      breaks = qu,
-      labels = names(qu)[-1], 
-      include.lowest = TRUE)
-}
-
-full_data <- full_data%>%#bind_rows(tibble(Year = c(2015:2040, 2060:2080))) %>% 
-  mutate(
-        Temp     = make_quantiles(Temp),
-        early_lb = ifelse(year < 2060, lb_kill, NA),
-        late_lb  = ifelse(year > 2060, lb_kill, NA), 
-        early_ub = ifelse(year < 2060, ub_kill, NA),
-        late_ub  = ifelse(year > 2060, ub_kill, NA))
-
-pred_smooth       <- loess(pred_kills ~ year, full_data)
-pred_early_smooth <- loess(early_kills ~ year, full_data %>% filter(year < 2070))
-pred_late_smooth  <- loess(late_kills ~ year, full_data %>% filter(year>2070))
-early_lb_smooth   <- loess(early_lb ~ year, full_data %>% filter(year < 2070))
-late_lb_smooth    <- loess(late_lb ~ year, full_data %>% filter(year>2070))
-early_ub_smooth   <- loess(early_ub ~ year, full_data %>% filter(year < 2070))
-late_ub_smooth    <- loess(late_ub ~ year, full_data %>% filter(year>2070))
-
-full_data_smoothed <- full_data %>%
-  mutate(
-    pred_s     = ifelse(year > 2040, pred_smooth$fitted, NA),
-    pred_e_s   = ifelse(year > 2040 & year < 2070, pred_early_smooth$fitted, NA),
-    pred_l_s   = ifelse(year > 2070, pred_late_smooth$fitted, NA),
-    early_lb_s = ifelse(year > 2040 & year < 2070, early_lb_smooth$fitted, NA),
-    late_lb_s  = ifelse(year> 2070, late_lb_smooth$fitted, NA),
-    early_ub_s = ifelse(year> 2040 & year < 2070, early_ub_smooth$fitted, NA),
-    late_ub_s  = ifelse(year> 2070, late_ub_smooth$fitted, NA)
-  )
-
-plot <- full_data_smoothed %>%
-  ggplot(aes(x=year,y = pred_s)) +
-  geom_rect(xmin = 2019, xmax = 2041, ymin = 0, ymax = 110, alpha = 0.002)+
-  geom_rect(xmin = 2059, xmax = 2081, ymin = 0, ymax = 110, alpha = 0.002)+
-  #geom_line(color = 'black') +
-  #geom_ribbon(aes(ymax = ub_kill, ymin = lb_kill), alpha = 0.5)+
-  geom_point(aes(y = actual_kills), color = 'black', alpha = 0.8)+
-  geom_line(aes(y = pred_e_s), color = 'black')+
-  geom_line(aes(y = pred_l_s), color = 'black')+
-  geom_line(aes(y = early_lb_s), color = 'black', linetype = 'dashed') +
-  geom_line(aes(y = late_lb_s), color = 'black', linetype = 'dashed') +
-  geom_line(aes(y = early_ub_s), color = 'black', linetype = 'dashed')+
-  geom_line(aes(y = late_ub_s), color = 'black', linetype = 'dashed')+
-  ylab("Total Predicted Summerkills")+
-  xlab(NULL) +
-  theme_tufte()+
-  theme(text = element_text(family = 'sans'))+
-  ylim(0,70) 
-plot
-
-side_plot <- full_data %>%
-  ggplot +
-  geom_bar(stat = 'identity', aes(x = year, y = 1, fill = Temp)) +
-  theme_tufte() +
-  theme(axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
-  theme(text = element_text(family = 'sans')) +
-  scale_color_brewer(palette =  'OrRd', aesthetics = 'fill', direction = 1)
-#  scale_fill_discrete(low = 'blue', high = 'red', limits = c(9, 16)) 
-
-side_plot
-
-plot_full <- grid.arrange(plot, side_plot)
-
-ggsave("summerkill_time_serise.png", plot, width = 8, height = 5)
-
+final_p <- p_timeseries + p_temp + plot_layout(ncol = 1, heights = c(15, 1))
+setwd("../../figures")
+ggsave("summerkill_timeseries.png", final_p, height = 4.5, width = 8)
