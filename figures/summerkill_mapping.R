128c80d0779a65b8aa2ee270c32e69ff2a06c6d2
Andrew Bray <abray@rstudio-dev.reed.edu>
2019-01-31 10:36:56 -0800

    add descriptive panel naes

--- figures/summerkill_mapping.R
+++ figures/summerkill_mapping.R
@@ -1,4 +1,4 @@
-
+# Load packages
 library(tidyverse)
 library(ggthemes)
 library(ggmap)
@@ -9,73 +9,74 @@ library(DescTools)
 library(spdep)
 library(rgdal)
 
+# Load data
+historical_data <- read_csv('../data/processed/historical_data.csv',
+                            col_types = list(wbic = col_character()))
+future_data     <- read_csv('../data/processed/future_data.csv',
+                            col_types = list(wbic = col_character()))
 
-
-
-dsn2 <- "../data/wisconsin_outline"
+# Load and process map data
+dsn2 <- "../data/raw/wisconsin_outline"
 ogrListLayers(dsn2)
-spatial_w <- readOGR(dsn2, layer = 'wisco_only')
+spatial_w  <- readOGR(dsn2, layer = 'wisco_only')
+map_data_w <- fortify(spatial_w) 
+bbox <- c(-92.9, 42.4, -87, 46.9) 
+wisconsin_map <- get_map(bbox, zoom = 7, maptype = 'toner-lines')
 
+# Extract model predictions
+# Lasso selected logistic model
+m1 <- glm(summerkill ~ population  + lat + season + temp,
+          data =  historical_data,
+          family = binomial)
+p_event <- predict(object = m1, 
+             newdata = future_data, 
+             type = "response")
 
-historical_data <- read_csv('../processed-data/historical_data.csv')
-
-future_data <- read_csv('../processed-data/future_data.csv')
-
-
-lasso_fit_1 <- read_rds("../models/lasso_fit_1")
-
-predictions_1 <- predict(object=lasso_fit_1, future_data,
-                         type = "prob") %>%
-  select(2)
-
-map_data_w <-fortify(spatial_w) 
-
-bbox =c(-92.9, 42.4, -87, 46.9) 
-Wisconsin_map <- get_map(bbox, zoom = 7, maptype = 'toner-lines')
-
-
-
-# building data
-
-
-
-future_predictions = future_data %>% bind_cols(predictions_1) %>% mutate(event = pos)
-
-fig3_data = bind_rows(historical_data, future_predictions) %>%
-  mutate(generation = ifelse(year > 2070, 'c', 'b')) %>%
-  mutate(generation = ifelse(generation == 'b' & year < 2040, 'a', generation))%>%
-  mutate(summerkill_binary = ifelse(is.na(summerkill) | summerkill == 0, 0 , 1)) %>%
-  mutate(event = ifelse(year> 2040, event, NA)) %>%
-  mutate(no_event_prob = 1 - event) %>%
-  group_by(wbic, generation) %>%
-  summarise(Prob = (1 - prod(no_event_prob)), 
-            Prob = ifelse(is.na(Prob), max(summerkill_binary), Prob), 
-            V1 = mean(lon), 
-            V2 = mean(lat))
+# Combine data for plot
+hist_data <- historical_data %>%
+  mutate(prob = summerkill) %>%
+  select(wbic, lat, lon, prob) %>%
+  group_by(wbic, lat, lon) %>%
+  summarize(prob = sum(prob)) %>%
+  mutate(prob = ifelse(prob >= 1, 1, 0)) %>%
+  add_column(generation = "Historical")
   
+fut_data <- future_data %>%
+  select(wbic, year, lat, lon) %>%
+  add_column(p_event) %>%
+  mutate(p_no_event = 1 - p_event,
+         generation = ifelse(year > 2070, "Late 21st Century", "Early 21st Century")) %>%
+  group_by(wbic, generation) %>%
+  summarise(prob = (1 - prod(p_no_event)), 
+            lon = mean(lon), 
+            lat = mean(lat))
+  
+map_event_data <- hist_data %>%
+  bind_rows(fut_data) %>%
+  arrange(prob) %>%
+  mutate(generation = factor(generation, levels = c("Historical",
+                                                    "Early 21st Century",
+                                                    "Late 21st Century")))
 
-# building map
+# Construct map
+map <- ggmap(wisconsin_map) + 
+  geom_point(data = map_event_data, 
+             aes(x = lon, y = lat, color = prob, alpha = prob), 
+             size = 0.6) +
+  scale_color_gradient(low = 'Blue', high = 'Red', limits = c(0, 1)) +
+  scale_alpha(limits = c(0,1), guide = "none") +
+  geom_path(data = map_data_w, aes(x = long, y = lat, group = group)) +
+  theme(text = element_text(family = 'sans')) +
+  facet_grid(. ~ generation) + 
+  theme_bw() +
+  theme(strip.background = element_rect(colour = "white", fill = "white")) +
+  guides(color = guide_colourbar(title.theme = element_text(size = 9),
+                                barwidth = .8)) +
+  labs(x = "Longitude",
+       y = "Latitude",
+       color = "Probability")
 
-
-
-map <- ggmap(Wisconsin_map) + 
-  geom_point(data = arrange(fig3_data, Prob), aes(x = V1, V2, color = Prob, alpha = Prob), size = 0.5)+
-  scale_color_gradient(low = 'Blue', high = 'Red',na.value = 'red', limits = c(0, 1)) +
-  #theme( panel.border = element_rect(colour = "black", fill=NA, size=3),
-        #plot.margin=unit(c(1,0.3,1,2), "cm")+
-  ylab('Latitude')+
-  xlab('Longitude')+
-  guides(alpha = FALSE) +
-  scale_alpha(limits = c(0,1))+
-  geom_path(data = map_data_w, aes(x = long, y = lat, group = group))  +
-  theme(text = element_text(family = 'sans'))+
-  theme_bw()+
-  facet_grid(.~generation) + 
-  theme(strip.background = element_blank())
-  facet_grid(.~generation) +
-  theme(strip.background = element_rect(colour = "black", fill = "white"))
 map
-
 
 ggsave("summerkill_mapping.png", map, width = 8, height = 5)
 
